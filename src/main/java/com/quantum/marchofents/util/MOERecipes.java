413677e15a0195d905a7f210872997c8eac45eeb
alex <alex@kali.quantum.com>
2024-01-09 11:57:46 -0700

    stuff

--- src/main/java/com/quantum/marchofents/util/MOERecipes.java
+++ src/main/java/com/quantum/marchofents/util/MOERecipes.java
@@ -8,9 +8,12 @@ import com.quantum.marchofents.init.MOEItems;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraft.client.Minecraft;
 import net.minecraft.init.Items;
 import net.minecraft.item.*;
+import lotr.common.LOTRMod;
+import lotr.common.recipe.LOTRRecipes;
 
 public class MOERecipes {
 	
@@ -20,14 +23,22 @@ public class MOERecipes {
 	
 	public static void createArnorRecipes() {
 		
-		GameRegistry.addRecipe(new ItemStack (MOEItems.longswordArnor), "  X", " X ", "Y  ", "X", Items.iron_ingot, "Y", Items.stick);
+		moeArnor.add(new ShapedOreRecipe (new ItemStack (MOEItems.longswordArnor), "  X", " X ", "Y  ", "X", Items.iron_ingot, "Y", Items.stick));
+
 		
 		
-		
+	}
+	public static void registerRecipes() {
+		LOTRRecipes.rangerRecipes.addAll(moeArnor);
+	}
+	
+	public static boolean isIron(ItemStack itemstack) {
+		return LOTRMod.isOreNameEqual(itemstack, "oreIron") || LOTRMod.isOreNameEqual(itemstack, "ingotIron");
 	}
 	
 	public static void preInit() {
 		createArnorRecipes();
+		registerRecipes();
 	}
 	
 	
